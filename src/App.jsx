693d6c8017c5bade84bbf6a316988f0b35009fbc
Tomasz Mazur <61616164+masa87@users.noreply.github.com>
2021-11-28 17:28:06 +0100

    dodane stylowanie

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Feedback />
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +17,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer">
           Learn React
-        </a>
-      </header>
+        </a> */}
+      {/* </header> */}
     </div>
   );
 }
--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import styles from "./Feedback.module.css";
 
 class Feedback extends Component {
   state = {
@@ -17,18 +18,18 @@ class Feedback extends Component {
   render() {
     const { good, neutral, bad } = this.state;
     return (
-      <div>
-        <h1>Please leave feedback</h1>
-        <ul>
-          <li>Good</li>
-          <li>Neutral</li>
-          <li>Bad</li>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Please leave feedback</h1>
+        <ul className={styles.btn_list}>
+          <li className={styles.btn_listItem}>Good</li>
+          <li className={styles.btn_listItem}>Neutral</li>
+          <li className={styles.btn_listItem}>Bad</li>
         </ul>
         <h1>Statistics</h1>
-        <ul>
-          <li>Good: {good}</li>
-          <li>Neutral: {neutral}</li>
-          <li>Bad: {bad}</li>
+        <ul className={styles.counter_list}>
+          <li className={styles.counter_listItem}>Good: {good}</li>
+          <li className={styles.counter_listItem}>Neutral: {neutral}</li>
+          <li className={styles.counter_listItem}>Bad: {bad}</li>
         </ul>
       </div>
     );
